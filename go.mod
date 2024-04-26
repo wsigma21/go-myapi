5ee5a777f0008c04e357c00a332690eeaf07313c
wsigma21 <wallstlbl@gmail.com>
2024-04-26 21:39:10 +0900

    update: chapter1-6

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wsigma21/go-myapi.git
 
 go 1.21.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,29 +4,58 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
+// /hello
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello, world!\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Hello, world!\n")
 }
+
+// /article
 func ArticleHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Article...\n")
 }
+
+// /article/list
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	queryMap := req.URL.Query()
+
+	var page int
+	if pages, ok := queryMap["page"]; ok && len(pages) > 0 {
+		var err error
+		page, err = strconv.Atoi(pages[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	resString := fmt.Sprintf("Article List (page=%d)\n", page)
+	io.WriteString(w, resString)
 }
+
+// /article/id
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
+
+// /article/nice
 func ArticleNiceHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Nice...\n")
 }
+
+// /comment
 func CommentHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Comment...\n")
 }
