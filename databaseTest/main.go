6af210615b8384fa5b9915ec6e62504a15d021c9
wsigma21 <wallstlbl@gmail.com>
2024-05-14 20:56:38 +0900

    update: chapter3-6

--- databaseTest/main.go
+++ databaseTest/main.go
@@ -20,29 +20,21 @@ func main() {
 	}
 	defer db.Close()
 
-	articleID := 1000
-	const sqlStr = `
-		select *
-		from articles
-		where article_id = ?;
-	`
-	row := db.QueryRow(sqlStr, articleID)
-	if err := row.Err(); err != nil {
-		fmt.Println(err)
-		return
+	article := models.Article{
+		Title:    "insert test",
+		Contents: "Can I insert data correctly?",
+		UserName: "saki",
 	}
-
-	var article models.Article
-	var createdTime sql.NullTime
-	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	const sqlStr = `
+		insert into articles (title, contents, username, nice, created_at) values
+		(?, ?, ?, 0, now());
+	`
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	if createdTime.Valid {
-		article.CreatedAt = createdTime.Time
-	}
-
-	fmt.Printf("%+v\n", article)
+	fmt.Println(result.LastInsertId())
+	fmt.Println(result.RowsAffected())
 }
