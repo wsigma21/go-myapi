5eeb29d8a14851be48523a440357ea4520679c20
wsigma21 <wallstlbl@gmail.com>
2025-07-07 22:21:59 +0900

    feat: 1-3まで

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wsigma21/go-mympi
+
+go 1.24.4
---
+++ handlers/handlers.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"io"
+	"net/http"
+	"fmt"
+)
+
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Hello, world!\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "positingArticle...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article List\n")
+	} else {
+		http.Error(w,"invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		articleID := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleID)
+		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Nice...")
+	} else {
+		http.Error(w,"invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
+func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Commment...")
+	} else {
+		http.Error(w,"invalid method", http.StatusMethodNotAllowed)
+	}
+}
