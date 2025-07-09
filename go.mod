d4ff7e7f13372ddb3d112785f574c718790ddcfc
wsigma21 <wallstlbl@gmail.com>
2025-07-09 22:01:03 +0900

    feat: Chapter1終わりまで

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wsigma21/go-mympi
 
 go 1.24.4
+
+require github.com/gorilla/mux v1.8.1
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,54 +4,50 @@ import (
 	"io"
 	"net/http"
 	"fmt"
+	"strconv"
+	"github.com/gorilla/mux"
 )
 
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Hello, world!\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Hello, world!\n")
 }
 
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "positingArticle...\n")
-	} else {
-		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "positingArticle...\n")
 }
 
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Article List\n")
+	queryMap := req.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
 	} else {
-		http.Error(w,"invalid method", http.StatusMethodNotAllowed)
+		page = 1
 	}
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
 }
 
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		articleID := 1
-		resString := fmt.Sprintf("Article No.%d\n", articleID)
-		io.WriteString(w, resString)
-	} else {
-		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	articleID := 1
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 	}
+	resString := fmt.Sprintf("Article No.%d\n", articleID)
+	io.WriteString(w, resString)
 }
 
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Nice...")
-	} else {
-		http.Error(w,"invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Nice...")
 }
 
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "Posting Commment...")
-	} else {
-		http.Error(w,"invalid method", http.StatusMethodNotAllowed)
-	}
+	io.WriteString(w, "Posting Commment...")
 }
