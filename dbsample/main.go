5942f707f54c7cbfc4054fc126eac4846741d45f
wsigma21 <wallstlbl@gmail.com>
2025-08-02 11:11:12 +0900

    feat: 3-4:Nullかもしれない値も受け取る

--- dbsample/main.go
+++ dbsample/main.go
@@ -20,7 +20,7 @@ func main() {
 	defer db.Close()
 
 	const sqlStr = `
-		select title, contents, username, nice
+		select *
 		from articles;
 	`
 
@@ -34,7 +34,12 @@ func main() {
 	articleArray := make([]models.Article, 0)
 	for rows.Next() {
 		var article models.Article
-		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+		var createdTime sql.NullTime
+		err := rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+
+		if createdTime.Valid {
+			article.CreatedAt = createdTime.Time
+		}
 
 		if err != nil {
 			fmt.Println(err)
