0863436b369d25942685df008ce604071c86d1d3
wsigma21 <wallstlbl@gmail.com>
2025-08-04 16:21:40 +0900

    feat: 3-6終了

--- dbsample/main.go
+++ dbsample/main.go
@@ -19,29 +19,24 @@ func main() {
 	}
 	defer db.Close()
 
-	articleID := 2
+	// データを挿入する処理
+	article := models.Article{
+		Title:    "insert test",
+		Contents: "Can I insert data correctly?",
+		UserName: "abe",
+	}
 	const sqlStr = `
-		select *
-		from articles
-		where article_id = ?;
+		insert into articles (title, contents, username, nice, created_at)
+		values
+		(?, ?, ?, 0, now());
 	`
 
-	row := db.QueryRow(sqlStr, articleID)
-	if err := row.Err(); err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	var article models.Article
-	var createdTime sql.NullTime
-
-	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	result, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	if createdTime.Valid {
-		article.CreatedAt = createdTime.Time
-	}
-	fmt.Printf("%+v\n", article)
+	// 結果を確認
+	fmt.Println(result.LastInsertId())
+	fmt.Println(result.RowsAffected())
 }
