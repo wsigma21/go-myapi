9c387e00a62445a172fd490b3d5085a669915292
wsigma21 <wallstlbl@gmail.com>
2025-08-07 11:27:55 +0900

    feat: SelectArticleListの作成

--- repositories/articles.go
+++ repositories/articles.go
@@ -7,6 +7,10 @@ import (
 	"github.com/wsigma21/go-mympi/models"
 )
 
+const (
+	articleNumPerPage = 5
+)
+
 // 新規投稿をデータベースにinsertする関数
 func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
 	const sqlStr = `
@@ -29,4 +33,33 @@ func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
 	newArticle.ID = int(id)
 
 	return newArticle, nil
+}
+
+// 変数pageで指定されたページに表示する投稿一覧をデータベースから取得する関数
+func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
+	const sqlStr = `
+		select article_id, title, contents, username, nice
+		from articles
+		limit ? offset ?;
+	`
+
+	rows, err := db.Query(sqlStr, articleNumPerPage, articleNumPerPage*(page-1))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	articleArray := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		if err != nil {
+			return nil, err
+		} else {
+			articleArray = append(articleArray, article)
+		}
+	}
+
+	return articleArray, nil
 }
