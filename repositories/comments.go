09e2af3822821478347dcd47c7b0309cd9a91d57
wsigma21 <wallstlbl@gmail.com>
2025-08-12 11:48:49 +0900

    feat: SelectCommentList作成

--- repositories/comments.go
+++ repositories/comments.go
@@ -27,3 +27,35 @@ func InsertComment(db *sql.DB, comment models.Comment) (models.Comment, error) {
 
 	return newComment, nil
 }
+
+// 指定IDの記事についたコメント一覧を取得する関数
+func SelectCommentList(db *sql.DB, articleID int) ([]models.Comment, error) {
+	const sqlStr = `
+	select *
+	from comments
+	where article_id = ?;
+	`
+
+	rows, err := db.Query(sqlStr, articleID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	commentArray := make([]models.Comment, 0)
+	for rows.Next() {
+		var comment models.Comment
+		var createdTime sql.NullTime
+		err := rows.Scan(&comment.CommentID, comment.ArticleID, comment.Message, createdTime)
+		if err != nil {
+			return nil, err
+		} else {
+			if createdTime.Valid {
+				comment.CreatedAt = createdTime.Time
+			}
+			commentArray = append(commentArray, comment)
+		}
+	}
+
+	return commentArray, nil
+}
