b1596dda8b4966e8dd91fff74da6d4aafe229788
wsigma21 <wallstlbl@gmail.com>
2025-09-04 07:44:10 +0900

    feat: GetArticleService作成

---
+++ services/helper.go
@@ -0,0 +1,26 @@
+package services
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// テスト全体で共有するsql.DB型
+var testDB *sql.DB
+
+var (
+	dbUser = "docker"
+	dbPassword = "docker"
+	dbDatabase = "sampledb"
+	dbConn = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+)
+
+func connectDB() error {
+	var err error
+	testDB, err = sql.Open("mysql", dbConn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
