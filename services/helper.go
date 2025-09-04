dee5696715a27e40c36043735860f6e23b75b65c
wsigma21 <wallstlbl@gmail.com>
2025-09-05 07:31:48 +0900

    feat: 環境変数の利用

--- services/helper.go
+++ services/helper.go
@@ -3,6 +3,8 @@ package services
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -10,9 +12,9 @@ import (
 var testDB *sql.DB
 
 var (
-	dbUser = "docker"
-	dbPassword = "docker"
-	dbDatabase = "sampledb"
+	dbUser = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("DB_PASSWORD")
+	dbDatabase = os.Getenv("DB_NAME")
 	dbConn = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 )
 
