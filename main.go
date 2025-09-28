7ccfe5fa38e892d6e40d90728b571a1dd60363b6
wsigma21 <wallstlbl@gmail.com>
2025-09-28 09:59:28 +0900

    refactor: 6-3 ルータ層切り分け

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 
-	"github.com/gorilla/mux"
 	"github.com/wsigma21/go-myapi/controllers"
+	"github.com/wsigma21/go-myapi/routers"
 	"github.com/wsigma21/go-myapi/services"
 )
 
@@ -30,15 +30,7 @@ func main() {
 
 	ser := services.NewMyAppService(db)
 	con := controllers.NewMyAppController(ser)
-
-	r := mux.NewRouter()
-	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}",
-		con.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
+	r := routers.NewRouter(con)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
